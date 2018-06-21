7724fb4579ac226e05c1d960f156d5f27404498a
Regenyi <regenyi.adam@gmail.com>
2018-06-21 14:21:36 +0200

    v2.0 - range and error handling implemented

--- src/nearby/ArrayChecker.java
+++ src/nearby/ArrayChecker.java
@@ -1,17 +1,54 @@
 package nearby;
 
-
-import java.util.Arrays;
-import java.util.stream.IntStream;
-
+import java.util.ArrayList;
 
 public class ArrayChecker {
     public int[] nearby(int x, int y, int range) {
+        if (x > multi.length) {
+            System.out.println("nincs ilyen sora a listának!");
+            return null;
+        }
+
         int[] row = multi[x];
-        int[] pos = Arrays.copyOfRange(row, y+1, y+1+range);
-        int[] neg = Arrays.copyOfRange(row, y-range, y);
-        int[] result = IntStream.concat(Arrays.stream(neg), Arrays.stream(pos)).toArray();
-        return result;
+        int arrayEnd = row.length-1;
+        ArrayList<Integer> result = new ArrayList<>();
+        //System.out.println("input: " + x + " " + y + " " + range);
+        //System.out.println("y = "+ row[y]);
+
+        if (y > arrayEnd) {
+            System.out.println("nincs ilyen indexű elem!");
+            return null;
+        }
+
+        if (y-range <= 0) {
+            //System.out.println("range átnyúlik minuszba");
+            for (int i = 0; i < ((y-range)+range); i++) {
+                result.add(row[i]);
+            }
+        } else {
+            //System.out.println("range lefele belefér a listába");
+            for (int i = y - range; i < y; i++) {
+                result.add(row[i]);
+            }
+        }
+        if (y+range >= arrayEnd && y < arrayEnd) {
+            //System.out.println("range túlnyúlik a listán");
+            for (int i = 0; i < (arrayEnd - y); i++) {
+                result.add(row[i+y+1]);
+            }
+        }
+        if (y+range < arrayEnd) {
+            //System.out.println("range felfele belefér a listába");
+            for (int i = 0; i < range; i++) {
+                result.add(row[i + y+1]);
+            }
+        }
+
+        int[] arr = new int[result.size()];
+        for(int i = 0; i < result.size(); i++) {
+            arr[i] = result.get(i);
+        }
+        return arr;
     };
 
     private int[][] multi = {
@@ -19,6 +56,4 @@ public class ArrayChecker {
             { 1, 3, 5, 7 },
             { 321, 320, 32, 3, 41241, -11, -12, -13, -66, -688 }
     };
-
-
 }
--- src/nearby/Nearby.java
+++ src/nearby/Nearby.java
@@ -5,9 +5,8 @@ import java.util.Arrays;
 public class Nearby {
 
     public static void main(String[] args){
-        ArrayChecker nearby1 = new ArrayChecker();
-        int[] result = nearby1.nearby(0,2, 2);
+        ArrayChecker list = new ArrayChecker();
+        int[] result = list.nearby(4,0, 18);
         System.out.println(Arrays.toString(result));
-
     }
 }
